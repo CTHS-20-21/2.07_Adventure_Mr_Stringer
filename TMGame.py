ab61652b77bd0fe8290bc4d100f80063c95bb304
Michael Stringer Jr <michael.stringer.2.0@gmail.com>
2020-09-24 08:51:21 -0500

    Update TMGame.py

--- TMGame.py
+++ TMGame.py
@@ -1,1 +1,16 @@
 #Make sure to use comments for documentation of execition flow
+#Greet the user
+print("Welcome to the dungeon! Available commands are left, right, up, down, grad and fight. ")
+
+#Variables
+done = False
+#Start the user at the beginning
+player_floor = 1
+player_position = 1
+
+#Start the turns
+
+while done == False:
+  print(yes)
+  done = True
+      
